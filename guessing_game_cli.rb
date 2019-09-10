beacee122df8a9e737d2ce761f61037d71c08131
Jacques Robert Plante <jonlypaffle@gmail.com>
2019-09-10 15:00:43 +0000

    Automatically backed up by Learn

--- guessing_game_cli.rb
+++ guessing_game_cli.rb
@@ -1,1 +1,29 @@
-# Code your solution here!
+def get_user_input
+  get.chomp
+end
+
+def generate_num
+  num = rand(1..6)
+end
+
+def goodbye_message
+  puts "Goodbye!"
+end
+
+
+
+
+
+
+
+
+
+
+def run_guessing_game
+  
+  
+  
+  
+  
+  goodbye_message
+end
