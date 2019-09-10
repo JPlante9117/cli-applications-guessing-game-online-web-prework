4b3e44c69af9bd1ed5681a34e32c87ef841b35de
Jacques Robert Plante <jonlypaffle@gmail.com>
2019-09-10 15:07:09 +0000

    Automatically backed up by Learn

--- guessing_game_cli.rb
+++ guessing_game_cli.rb
@@ -6,8 +6,22 @@ def generate_num
   num = rand(1..6)
 end
 
+def prompt_user
+  puts "Guess a number 1 through 6!"
+end
+
 def goodbye_message
   puts "Goodbye!"
+end
+
+def compare_answers(num)
+  num = generate_num
+  input = get_user_input
+  
+  if input == num
+    return "You guessed the correct number!"
+  else
+    return "Sorry! The computer guessed #{num}."
 end
 
 
@@ -17,11 +31,12 @@ end
 
 
 
-
-
 def run_guessing_game
+  generate_num
+  prompt_user
   get_user_input
   
+  compare_answers(num)
   
   if get_user_input == "exit"
     goodbye_message
