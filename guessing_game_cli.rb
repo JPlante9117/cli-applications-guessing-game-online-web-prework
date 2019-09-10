54156a814276c58996a808469a6bf953d6356f73
Jacques Robert Plante <jonlypaffle@gmail.com>
2019-09-10 15:40:09 +0000

    Automatically backed up by Learn

--- guessing_game_cli.rb
+++ guessing_game_cli.rb
@@ -1,6 +1,6 @@
 require 'pry'
 
-=begin def get_user_input
+def get_user_input
   gets.chomp.to_s
 end
 
@@ -24,26 +24,3 @@ input = get_user_input
     puts "You guessed the correct number!"
   end
 end
-=end
-
-def run_guessing_game
-  # Take input from CL
-  puts "Guess a number between 1 and 6."
-  user_input = gets.chomp
-  comp_num = rand(1..6)
-  while user_input != "exit" do
-    # Compare input to random num
-    # Print result: correct?
-    if user_input.to_i == comp_num
-      puts "You guessed the correct number!"
-    else
-      puts "The computer guessed #{comp_num}."
-    end
-    comp_num = rand(1..6)
-    user_input = gets.chomp
-  end
-  # Exit if input == exit
-  if user_input == "exit"
-    puts "Goodbye!"
-  end
-end 
